847ce1d7bfbd2ebf32d12784365fdd567a898b3a
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2023-03-19 10:27:12 +0530

    initial commit

---
+++ src/test/java/io/github/sarthaka24/neo4jdemo/Neo4JDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sarthaka24.neo4jdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Neo4JDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
