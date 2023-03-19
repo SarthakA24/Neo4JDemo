45abdf93c634b905f4cff168eb983ec90cdf43d2
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2023-03-19 10:48:18 +0530

    added @EnableNeo4jRepositories annotation

--- src/main/java/io/github/sarthaka24/neo4jdemo/Neo4JDemoApplication.java
+++ src/main/java/io/github/sarthaka24/neo4jdemo/Neo4JDemoApplication.java
@@ -2,8 +2,10 @@ package io.github.sarthaka24.neo4jdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
 @SpringBootApplication
+@EnableNeo4jRepositories
 public class Neo4JDemoApplication {
 
     public static void main(String[] args) {
